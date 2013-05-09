8dbc3c170764b2c095b17f34ffda504626430202
jjmr <jjmr83@gmail.com>
2013-05-09 08:48:40 +0200

    use async and remove the ugly doom pyramid

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	db: {
 		name: 'watcher',
-		host_port: '10.95.254.246:27017',
-		//host_port: 'localhost:27017',
+	//	host_port: '10.95.254.246:27017',
+		host_port: 'localhost:27017',
 		collection: 'notification'
 	},
 	popbox: {
